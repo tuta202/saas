54da6232bc1c751f3eb36d0e38766e3f04ef62c4
Tú Trần Anh <trananhtu171002@gmail.com>
2025-10-31 23:18:39 +0700

    push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from '@clerk/nextjs';
 import type { AppProps } from 'next/app';
-import "@/styles/globals.css";
+import 'react-datepicker/dist/react-datepicker.css';
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
